b98f50a757f891effd89b416b2d0b65a8589d641
Felix Kaaman <trundmatu@gmail.com>
2016-12-04 14:42:53 +0100

    Add day 4

---
+++ day4/part2.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool valid(int occurrences[26], char *ptr) {
+    int prev = 0;
+
+    for (int i = 0; i < 5; ++i) {
+        char m = *ptr++;
+        prev |= (1 << (m - 'a'));
+
+        for (int j = 0; j < 26; ++j) {
+            if (occurrences[j] > occurrences[m - 'a'] && !(prev & (1 << j))) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main(int argc, char *argv[argc + 1]) {
+    char line[256];
+    int sum = 0;
+
+    while (fgets(line, sizeof(line), stdin)) {
+        int occurrences[26] = {0};
+        int sector = 0;
+
+        char *p = line;
+        char c;
+
+        while ((c = *p++)) {
+            if (c == '-')
+                continue;
+
+            if (c > '9') {
+                occurrences[c - 'a']++;
+            } else {
+                sector +=    (c - '0') * 100;
+                sector += (*p++ - '0') * 10;
+                sector += (*p++ - '0');
+
+                (void)p++;
+
+                if (valid(occurrences, p))
+                    sum += sector;
+
+                break;
+            }
+        }
+
+        int shift = sector % 26;
+        for (char *s = line; *s; ++s) {
+            if (*s == '-')
+                *s = ' ';
+            else if (*s > '9')
+                *s = 'a' + (((*s - 'a') + shift) % 26);
+            else
+                break;
+        }
+
+        if (strstr(line, "northpole object storage"))
+            printf("%d\n", sector);
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
